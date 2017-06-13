44433ad42107033dbce20e876553b74f0adb81c1
bstroshek <bstroshek@gmail.com>
2017-06-13 13:14:21 +0300

    about2

---
+++ js/common.js
@@ -0,0 +1,90 @@
+$(function() {
+
+
+    // SLICK
+
+    $('.single-item').slick({
+        adaptiveHeight: true
+    });
+
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '0',
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        variableWidth: true,
+        // respondTo: 'slider',
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    // FANCYBOX
+
+    $("[data-fancybox]").fancybox({
+        // Options will go here
+    });
+
+
+    $("#test").on('click', function() {
+
+        $.fancybox.open([
+            {
+                src  : '../img/6new-large.jpg',
+                opts : {
+                    // caption : 'First caption'
+                }
+            },
+            {
+                src  : '../img/160-large.jpg',
+                opts : {
+                    // caption : 'First caption'
+                }
+            },
+            {
+                src  : '../img/7new-large.jpg',
+                opts : {
+                    // caption : 'Second caption'
+                }
+            }
+        ], {
+            loop : true,
+            hash : "test"
+        });
+
+    });
+
+    // PLAY VIDEO
+
+
+    var controls = {
+        video: $(".bottom-video"),
+        playpause: $(".playpause")
+    };
+    var video = controls.video[0];
+    controls.playpause.click(function(){
+            video.play();
+            $(this).remove();
+    });
+
+
+
+});
